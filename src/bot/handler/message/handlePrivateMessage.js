f7e29b7dffe89a15b98a820a894fe7257a6b7279
Lucas Valandro <lvalandrorocha@hotmail.com>
2019-03-14 17:38:12 -0300

    [PRIVATE-HANDLE-TOPICS]
    Handle topics in a private conversation with the bot.

--- src/bot/handler/message/handlePrivateMessage.js
+++ src/bot/handler/message/handlePrivateMessage.js
@@ -1,9 +1,23 @@
 const handleAnthem = require('../anthem/handleAnthem');
+const handleTopic = require('../topic/handleTopic');
 
 module.exports = (data, bot, params, client) => {
     const anthemRe = /hino/g;
+    const peRe = /pe(?!.)/g;
+    const psRe = /ps(?!.)/g;
     const msg = data.text;
+    const db = client.db('fejers');
     if(anthemRe.test(msg)) {
         handleAnthem(bot, params, data.channel);
+    } else if(psRe.test(msg)) {
+        db.collection('bot').find({'topic.id': '2'}).toArray(function(err, info) {
+            if(err) console.log(err);
+            handleTopic(bot, params, data.channel, info);
+        });  
+    } else if(peRe.test(msg)) {
+        db.collection('bot').find({'topic.id': '3'}).toArray(function(err, info) {
+            if(err) console.log(err);
+            handleTopic(bot, params, data.channel, info);
+        });  
     }
 }
